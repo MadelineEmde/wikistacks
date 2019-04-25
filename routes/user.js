b25befa0fab12a9591a8327072643faa67385299
Madeline Emde <madeline.emde@gmail.com>
2019-04-25 17:03:31 -0400

    generate slug working

--- routes/user.js
+++ routes/user.js
@@ -1,24 +1,23 @@
 const router = require("express").Router();
 
-router.get('/users/', (req, res, next) => {
-    res.send("testing users")
+router.get("/users/", (req, res, next) => {
+  res.send("testing users");
 });
 
-// router.get('/users/:id', (req, res, next) => {
-//     res.send('testing specific user with params')
-// });
+router.get("/users/:id", (req, res, next) => {
+  res.send("testing specific user with params");
+});
 
-// router.post('/users', (req, res, next) => {
-//     res.send('users post')
-// });
+router.post("/users", (req, res, next) => {
+  res.send("users post");
+});
 
-// router.put('/users/:id', (req, res, next) => {
-//     res.send('test put users id')
-// });
+router.put("/users/:id", (req, res, next) => {
+  res.send("test put users id");
+});
 
-// router.delete('/users/:id', (req, res, next) => {
-//     res.send('delete user test')
-// });
-
+router.delete("/users/:id", (req, res, next) => {
+  res.send("delete user test");
+});
 
 module.exports = router;
--- routes/wiki.js
+++ routes/wiki.js
@@ -1,13 +1,33 @@
 const router = require("express").Router();
 const layout = require("../views/layout");
 const addPage = require("../views/addPage");
+const Page = require("../models/index").Page;
 
 router.get("/", (req, res, next) => {
   res.send(layout("test here"));
 });
 
-router.post("/", (req, res, next) => {
-  res.send("test post");
+function titleToSlug(str) {
+  let slug = str.replace(/\s+/g, "_").replace(/\W/g, "");
+  console.log(slug);
+}
+titleToSlug("what the fuck is regExp");
+router.post("/", async (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+
+  const page = new Page({
+    title,
+    content
+  });
+
+  try {
+    await page.save();
+    res.redirect("/");
+  } catch (error) {
+    next(error);
+  }
+  // res.json(submitted);
 });
 
 router.get("/add", (req, res, next) => {
